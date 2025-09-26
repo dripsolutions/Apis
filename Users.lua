df50bf87c39af57cb3d8cee79891535fef1f70ba
dripsolutions <fcsteikee@gmail.com>
2025-09-25 22:54:23 -0300

    Update Users.lua

--- Users.lua
+++ Users.lua
@@ -1,8 +1,4 @@
 allowedUsers = {
-    ["games"] = true,   --Tempo: 10/09,
-    ["infor"] = true,   --Tempo: 09/10 ✓,
-    ["ronoazoro"] = true,   --Tempo: 09/10 09/19,
-    ["levono"] = true,   --Tempo: 90
     
 }
 
